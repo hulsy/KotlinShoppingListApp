6189110d72810b13a8719e57909543fabd8f0cb2
Alex Hulford <hulf0002@algonquinlive.com>
2023-08-20 09:54:22 -0400

    added room database and defined entities

---
+++ app/src/main/java/com/example/learningkotlin/ShoppingDB.kt
@@ -0,0 +1,29 @@
+package com.example.learningkotlin
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [ShoppingItem::class],
+    version = 1
+)
+abstract class ShoppingDB : RoomDatabase(){
+
+    abstract fun getShoppingDao(): ShoppingDao
+
+    companion object {
+        @Volatile
+        private var instance: ShoppingDB? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: createDB(context).also { instance = it }
+        }
+
+        private fun createDB(context: Context) =
+            Room.databaseBuilder(context.applicationContext,
+                ShoppingDB::class.java, "ShoppingDB.db").build()
+    }
+}
